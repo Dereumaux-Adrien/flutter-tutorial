4f5ae745a1fda49dbfe8c6bf4c35cb88b7a43000
DESKTOP-BOR4GSQ\Dereumaux-Adrien <dereumauxadrien@gmail.com>
2021-09-06 22:12:42 +0200

    Added export of children_layout

--- lib/app_tutorial.dart
+++ lib/app_tutorial.dart
@@ -1,5 +1,6 @@
 library tutorial;
 
-export 'src/models/tutorial_item.dart';
 export 'src/controller/tutorial_tutorial.dart';
+export 'src/models/children_layout.dart';
 export 'src/models/shape_models.dart';
+export 'src/models/tutorial_item.dart';
